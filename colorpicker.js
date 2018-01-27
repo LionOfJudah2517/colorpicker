0243885f68770be3202afef93866afe476a08d32
Scott O'Neil <ScottO@scott.home>
2018-01-27 07:43:02 -0600

    Working out the logic for the game with JS

--- colorpicker.js
+++ colorpicker.js
@@ -1,5 +1,5 @@
-
 var colors = [
+
 ];
 
 var squares = document.querySelectorAll(".squares");
@@ -7,16 +7,19 @@ var r;
 var g;
 var b;
 var rgb;
-var buttons = document.getElementsByTagName("button");
+var button = document.querySelectorAll(".button");
 
+ button[0].addEventListener("click", () =>{
+  this.style.backgroundColor = "steelblue"
+}, false)
 
 //Button functionality
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", () => {
-    buttons[i].style.backgroundColor = "steelblue"
-    }
-  )
-}
+// for (var i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", () => {
+//     buttons[i].style.backgroundColor = "steelblue"
+//     }
+//   )
+// }
 
 
 //Random colors that are generated to the squares.
@@ -26,5 +29,10 @@ for (var i = 0; i < buttons.length; i++) {
      var b = Math.floor(Math.random()*256);
      var rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
   let color = squares[i].style.backgroundColor = rgb
-  colors.push(color)
+  colors.push(color);
     }
+
+var pickColor = colors[2];
+
+var rgbSet = document.getElementById("RGBCO");
+rgbSet.innerHTML = pickColor;
